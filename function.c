529dc5889186ecd3e4dde4e9544eeedecbcf5e6d
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2021-03-17 22:56:08 +0300

    Add files via upload

---
+++ function.c
@@ -0,0 +1,16 @@
+// Copyright 2021 Mary Zyryanova.
+#include <stdio.h>
+#include <math.h>
+#include "function.h"
+
+double function(int n, double f) {
+    double answer = 1.0;
+    for (int i = 1; i <= n; i++) {
+        double current = 0.;
+        for (int k = 0; k <= i; k++) {
+            current += f + k;
+        }
+        answer *= 1. / current;
+    }
+    return answer;
+}
