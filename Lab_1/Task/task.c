79076999f0a057c1fca0d03e4f4d1b830569f5bf
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2021-03-17 03:13:43 +0300

    New-files

--- Lab_1/Task/task.c
+++ Lab_1/Task/task.c
@@ -1,25 +1,13 @@
 // Copyright 2021 Mary Zyryanova.
 #include <stdio.h>
-#include <math.h>
+#include "mylib/function.h"
 
 int main(void) {
     int n;
     double f;
     char a = 'y';
     while (a != 'n') {
-        printf("n = ");
-        scanf("%d", &n);
-        printf("f = ");
-        scanf("%lf", &f);
-        double answer = 1.0;
-        for (int i = 1; i <= n; i++) {
-            double current = 0;
-            for (int k = 0; k <= i; k++) {
-                current += f + k;
-            }
-            answer *= 1. / current;
-        }
-        printf("p = %g", answer);
+        printf("p = %f", Function(n, f));
         printf("\nDo you want to continue?\nAnswer: ");
         scanf("%s", &a);
     }
