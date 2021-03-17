bbaa57a4bf037ca21f13cb1f0b21e5b8b3f218a5
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2021-03-17 11:08:57 +0300

    Add files via upload

---
+++ Lab_1/Task/function.c
@@ -0,0 +1,20 @@
+// Copyright 2021 Mary Zyryanova.
+#include <stdio.h>
+#include <math.h>
+#include "function.h"
+
+double Function(int n, double f) {
+    printf("\nn = ");
+    scanf("%d", &n);
+    printf("f = ");
+    scanf("%lf", &f);
+    double answer = 1.0;
+    for (int i = 1; i <= n; i++) {
+        double current = 0;
+        for (int k = 0; k <= i; k++) {
+            current += f + k;
+        }
+        answer *= 1. / current;
+    }
+    return answer;
+}
---
+++ Lab_1/Task/function.h
@@ -0,0 +1,5 @@
+// Copyright 2021 Mary Zyryanova.
+#ifndef LAB_1_TASK_1_FUNCTION_H_
+#define LAB_1_TASK_1_FUNCTION_H_
+#endif  // LAB_1_TASK_1_FUNCTION_H_
+double Function(int n, double f);
