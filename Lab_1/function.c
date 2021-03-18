053cc27c831b93d1d2431eae602e4debe4f31a67
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2021-03-18 10:55:24 +0300

    Update function.c

--- Lab_1/function.c
+++ Lab_1/function.c
@@ -4,9 +4,11 @@
 
 double function(int n, double f) {
     double answer = 1.0;
-    for (int i = 1; i <= n; i++) {
+    int i;
+    for (i = 1; i <= n; i++) {
         double current = 0.;
-        for (int k = 0; k <= i; k++) {
+        int k;
+        for (k = 0; k <= i; k++) {
             current += f + k;
         }
         answer *= 1. / current;
