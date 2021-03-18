7a3caebd0acfded3ad080d3ee05636871098acea
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2021-03-18 11:03:44 +0300

    Update task_test.c

--- Lab_1/task_test.c
+++ Lab_1/task_test.c
@@ -1,13 +1,16 @@
 #include "function.h"
 #include <assert.h>
+#include <float.h>
+#include <stdio.h>
 
 double test_function() {
-    assert(function(1, 1)) = 3.33333333333333314830e-01;
-    assert(function(12, -0.02)) = 1.55670422719344693713e-15;
-    assert(function(100, 10.102930)) = 3.39962140661823682055e-311;
+    assert(function(1, 1) - 3.33333333333333314830e-01 < 0.0000000000001);
+    assert(function(12, -0.02) - 1.55670422719344693713e-15< 0.0000000000001);
+    assert(function(100, 10.102930) - 3.39962140661823682055e-311< 0.0000000000001);
 }
 
 #undef main
 int main() {
     test_function();
+    printf("Scooby-dooby-dooooooooooooo");
 }
