adbd9ebed00873395d35fb917631901498d12261
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2021-03-23 00:50:36 +0300

    Delete Lab2_Task1 directory

--- Lab2_Task1/function.h
+++
@@ -1,8 +0,0 @@
-void clrscr();
-void presentData(float distance, unsigned int year, float value, float cost);
-void Menu();
-int EnterTheNumber(unsigned int number);
-float CostOfTheDistillation(float distance);
-float CostOfTheCustoms(unsigned int year, float value);
-float TotalCostInByn(float cost, float distance, unsigned int year, float value);
-void Information() ;
