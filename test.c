5725a82c1133e710f4b2dd1af90cd6c5c4d9e91c
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2021-03-23 23:52:08 +0300

    Update test.c

--- test.c
+++ test.c
@@ -4,9 +4,9 @@
 #undef main
 int main()
 {
-    assert(CountFactorialRec(2) - 2 < 0.01);
-    assert(CountFactorialIter(3) - 6 < 0.01);
-    assert(CountRightRec(2, 30) - 0.475750 < 0.01);
-    assert(CountRightIter(3, 60) - 0.748360 < 0.01);
+    assert(CountFactorialRec(2) == 2);
+    assert(CountFactorialIter(3) == 6);
+    assert(CountRightRec(2, 30) - 0.475750 < 0.00000001);
+    assert(CountRightIter(3, 60) - 0.748360 < 0.0000001);
     printf("Scooooooooooby-Doooooooby-Doooooo");
 }
