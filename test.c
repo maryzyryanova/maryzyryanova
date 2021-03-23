71e829b1f44dec5880fd8c1904e26373a12fa175
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2021-03-23 20:21:43 +0300

    Update test.c

--- test.c
+++ test.c
@@ -1,34 +1,13 @@
 #include <assert.h>
 #include "functions.h"
-#include <float.h>
-#include <stdio.h>
 #include <math.h>
-/*
-float Test_1() {
-    assert(fabs(CostOfTheDistillation(2453.2) - 3814.73) < 1);
-    assert(fabs(CostOfTheDistillation(7636) - 11873.98) < 1);
-}
 
-float Test_2() {
-    assert(fabs(CostOfTheCustoms(2, 5500) - 10263) < 1);
-    assert(fabs(CostOfTheCustoms(5, 2000) - 3732) < 1);
-    assert(fabs(CostOfTheCustoms(7, 3000) - 2799) < 1);
-    assert(fabs(CostOfTheCustoms(10, 3200) - 5971.2) < 1);
-    assert(fabs(CostOfTheCustoms(14, 1600) - 9952) < 1);
-}
-
-float Test_3() {
-    assert(fabs(TotalCostInByn(2000, 2453.2, 2, 5500) - 20297.73) < 1);
-    assert(fabs(TotalCostInByn(3000, 7636, 7, 3000) - 24002.98) < 1);
-    assert(fabs(TotalCostInByn(7000, 4500, 14, 1600) - 38719.50) < 1);
-}
-*/
 #undef main
 int main() {
     double test = 0.01;
     printf("%f\n", CostOfTheDistillation(20) - 31.10);
-    assert(CostOfTheDistillation(20) - 31.10 < test);
-    assert(CostOfTheCustoms(2, 5500) - 10263 < test);
-    assert(TotalCostInByn(2000, 2453.2, 2, 5500) - 20297.73 < test);
-    printf("Great job!!!");
+    assert(fabs(CostOfTheDistillation(20) - 31.10) < test);
+    assert(fabs(CostOfTheCustoms(2, 5500) - 10263) < test);
+    assert(fabs(TotalCostInByn(2000, 2453.2, 2, 5500) - 20297.73) < test);
+    return 0;
 }
