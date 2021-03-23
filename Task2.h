24861a299aa81eecbc552861035e89c83ce79b41
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2021-03-23 21:00:49 +0300

    Add files via upload

---
+++ Task2.h
@@ -0,0 +1,4 @@
+unsigned long int CountFactorialRec(unsigned long int n);
+unsigned long int CountFactorialIter(unsigned long int n) ;
+float CountRightRec(unsigned long int n, float angle);
+float CountRightIter(unsigned long int n, float angle);
