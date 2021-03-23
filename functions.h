b0929b72c179a9820762f83cad7f57b221642f2e
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2021-03-24 01:57:31 +0300

    Update functions.h

--- functions.h
+++ functions.h
@@ -1,6 +1,4 @@
-#include <stdint.h>
-
-int64_t CountFactorialRec(unsigned int n);
-int64_t CountFactorialIter(unsigned int n) ;
+unsigned int CountFactorialRec(unsigned int n);
+unsigned int CountFactorialIter(unsigned int n) ;
 float CountRightRec(unsigned int n, float angle);
 float CountRightIter(unsigned int n, float angle);
