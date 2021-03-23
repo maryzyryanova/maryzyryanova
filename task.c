cb054142be77380b711956f3a751c51b5b65dd84
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2021-03-23 20:34:58 +0300

    Update task.c

--- task.c
+++ task.c
@@ -1,1 +1,11 @@
+#include <assert.h>
+#include <stdio.h>
+#include "functions.h"
 
+#undef main
+int main() {
+  assert(fabs(CostOfTheDistillation(10) - 15.55) < 0.01);
+  assert(fabs(CostOfTheCustoms(2, 3300) - 3.73) < 0.01);
+  assert(fabs(TotalCostInByn(10000, 10, 2, 3300) - 31119.28) < 0.01);
+  return 0;
+}
