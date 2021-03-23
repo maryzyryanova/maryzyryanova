122f0bfba32a1673bff10e385119c1f42321989b
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2021-03-23 13:43:20 +0300

    Update test.c

--- test.c
+++ test.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <math.h>
 
-double Test_1() {
+float Test_1() {
     assert(CostOfTheDistillation(2453.2) - 3814.73 < 0.01);
     assert(CostOfTheDistillation(7636) - 11873.98 < 0.01);
     assert(CostOfTheDistillation(4500) - 6997.50 < 0.01);
