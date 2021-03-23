3ae60481119c77f5a6809f5abe2da3fe5cc1baa3
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2021-03-23 14:42:47 +0300

    Update test.c

--- test.c
+++ test.c
@@ -10,17 +10,17 @@ float Test_1() {
 }
 
 float Test_2() {
-    assert(CostOfTheCustoms(2, 5500) - 10263 < 0.01);
-    assert(CostOfTheCustoms(5, 2000) - 3732 < 0.01);
-    assert(CostOfTheCustoms(7, 3000) - 2799 < 0.01);
-    assert(CostOfTheCustoms(10, 3200) - 5971.2 < 0.01);
-    assert(CostOfTheCustoms(14, 1600) - 9952 < 0.01);
+    assert(CostOfTheCustoms(2, 5500) - 10263 < 1);
+    assert(CostOfTheCustoms(5, 2000) - 3732 < 1);
+    assert(CostOfTheCustoms(7, 3000) - 2799 < 1);
+    assert(CostOfTheCustoms(10, 3200) - 5971.2 < 1);
+    assert(CostOfTheCustoms(14, 1600) - 9952 < 1);
 }
 
 float Test_3() {
-    assert(TotalCostInByn(2000, 2453.2, 2, 5500) - 20297.73 < 0.01);
-    assert(TotalCostInByn(3000, 7636, 7, 3000) - 24002.98 < 0.01);
-    assert(TotalCostInByn(7000, 4500, 14, 1600) - 38719.50 < 0.01);
+    assert(TotalCostInByn(2000, 2453.2, 2, 5500) - 20297.73 < 1);
+    assert(TotalCostInByn(3000, 7636, 7, 3000) - 24002.98 < 1);
+    assert(TotalCostInByn(7000, 4500, 14, 1600) - 38719.50 < 1);
 }
 
 #undef main
