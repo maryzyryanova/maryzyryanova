06c18f5f6cbe359ef81b86d9e9c8795cafa7d5e2
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2021-03-23 14:50:26 +0300

    Update test.c

--- test.c
+++ test.c
@@ -5,22 +5,22 @@
 #include <math.h>
 
 float Test_1() {
-    assert(CostOfTheDistillation(2453.2) - 3814.73 < 1);
-    assert(CostOfTheDistillation(7636) - 11873.98 < 1);
+    assert(fabs(CostOfTheDistillation(2453.2) - 3814.73) < 1);
+    assert(fabs(CostOfTheDistillation(7636) - 11873.98) < 1);
 }
 
 float Test_2() {
-    assert(CostOfTheCustoms(2, 5500) - 10263 < 1);
-    assert(CostOfTheCustoms(5, 2000) - 3732 < 1);
-    assert(CostOfTheCustoms(7, 3000) - 2799 < 1);
-    assert(CostOfTheCustoms(10, 3200) - 5971.2 < 1);
-    assert(CostOfTheCustoms(14, 1600) - 9952 < 1);
+    assert(fabs(CostOfTheCustoms(2, 5500) - 10263) < 1);
+    assert(fabs(CostOfTheCustoms(5, 2000) - 3732) < 1);
+    assert(fabs(CostOfTheCustoms(7, 3000) - 2799) < 1);
+    assert(fabs(CostOfTheCustoms(10, 3200) - 5971.2) < 1);
+    assert(fabs(CostOfTheCustoms(14, 1600) - 9952) < 1);
 }
 
 float Test_3() {
-    assert(TotalCostInByn(2000, 2453.2, 2, 5500) - 20297.73 < 1);
-    assert(TotalCostInByn(3000, 7636, 7, 3000) - 24002.98 < 1);
-    assert(TotalCostInByn(7000, 4500, 14, 1600) - 38719.50 < 1);
+    assert(fabs(TotalCostInByn(2000, 2453.2, 2, 5500) - 20297.73) < 1);
+    assert(fabs(TotalCostInByn(3000, 7636, 7, 3000) - 24002.98) < 1);
+    assert(fabs(TotalCostInByn(7000, 4500, 14, 1600) - 38719.50) < 1);
 }
 
 #undef main
