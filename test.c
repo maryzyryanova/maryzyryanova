b79f46cae65913f1523318f704ae42ea69bc0835
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2021-03-24 00:10:06 +0300

    Update test.c

--- test.c
+++ test.c
@@ -5,7 +5,7 @@
 #undef main
 int main()
 {
-    assert(fabs(CountRightRec(2, 30) - 0.475750) < 0.000001);
-    assert(fabs(CountRightIter(3, 60) - 0.748360) < 0.000001);
+    assert(fabs(CountRightRec(2, 30) - 0.48) < 0.01);
+    assert(fabs(CountRightIter(3, 60) - 0.75) < 0.01);
     printf("Scooooooooooby-Doooooooby-Doooooo");
 }
