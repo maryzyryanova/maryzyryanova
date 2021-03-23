c7c2802fcdecb16c58ca2ee127648c2cdb82e15a
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2021-03-23 22:57:04 +0300

    Delete Task2.h

--- Task2.h
+++
@@ -1,4 +0,0 @@
-unsigned long int CountFactorialRec(unsigned long int n);
-unsigned long int CountFactorialIter(unsigned long int n) ;
-float CountRightRec(unsigned long int n, float angle);
-float CountRightIter(unsigned long int n, float angle);
