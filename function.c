9ad508e044b201ced4426eac41d3dac2833107b2
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2021-03-23 11:23:03 +0300

    Update and rename functions.c to function.c

--- function.c
+++ function.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "functions.h"
+#include "function.h"
+#include <math.h>
 
 void clrscr()
 {
@@ -9,9 +10,9 @@ void clrscr()
 
 void presentData(float distance, unsigned int year, float value, float cost)
 {
-    printf("The cost of the distillation of the car: %f", 3.11 * CostOfTheDistillation(distance));
-    printf("\nThe cost of the customs of the car: %f", 3.11 * CostOfTheCustoms(year, value));
-    printf("\nThe total cost of the car (in byn): %f\n", TotalCostInByn(cost, distance, year, value));
+    printf("The cost of the distillation of the car: %.2f", CostOfTheDistillation(distance));
+    printf("\nThe cost of the customs of the car: %.2f", CostOfTheCustoms(year, value));
+    printf("\nThe total cost of the car (in byn): %.2f\n", TotalCostInByn(cost, distance, year, value));
 }
 
 void Menu()
@@ -39,8 +40,9 @@ int EnterTheNumber(unsigned int number)
 
 float CostOfTheDistillation(float distance)
 {
-    float distillation = 0.5 * distance;
-    return distillation;
+    float distillation = 3.11 * 0.5 * distance;
+    float result = roundf(distillation * 10000) / 10000;
+    return result;
 }
 
 float CostOfTheCustoms(unsigned int year, float value)
@@ -48,34 +50,36 @@ float CostOfTheCustoms(unsigned int year, float value)
     float customs;
     if(year < 3)
     {
-        customs = 0.6 * value;
+        customs = 0.6 * 3.11 * value;
     }
     if (year >= 3 && year < 10)
     {
         if (value > 2500)
         {
-            customs = 0.3 * value;
+            customs = 0.3 * 3.11 *value;
         }
         else
         {
-            customs = 0.6 * value;    
+            customs = 0.6 * 3.11 * value;    
         }
     }
     if (year >= 10 && year < 14)
     {
-        customs = 0.6 * value;
+        customs = 0.6 * 3.11 * value;
     }
     if (year >= 14)
     {
-        customs = 2.0 * value;
+        customs = 2.0 * 3.11 * value;
     }
-    return customs;
+    float result = roundf(customs * 10000) / 10000;
+    return result;
 }
 
 float TotalCostInByn(float cost, float distance, unsigned int year, float value)
 {
-    float total = (cost + CostOfTheDistillation(distance) + CostOfTheCustoms(year, value)) * 3.11;
-    return total; 
+    float total = 3.11 * cost + CostOfTheDistillation(distance) + CostOfTheCustoms(year, value);
+    float result = roundf(total * 10000) / 10000;
+    return result;
 }
 
 void Information() 
