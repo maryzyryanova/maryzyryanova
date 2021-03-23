7dfac11bbf6fbe7bbfd1b183d5f0caac98f6beb2
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2021-03-24 02:17:56 +0300

    Update test.c

--- test.c
+++ test.c
@@ -5,9 +5,9 @@
 
 #undef main
 int main() {
-  printf("%f", CountRightRec(2, 0.523333));
+  printf("a: %f", CountRightRec(2, 0.523333));
   assert(fabs(CountRightRec(2, 0.523333) - 0.475557) < 0.1);
-  printf("%f", CountRightIter(3, 1.046667));
+  printf("b: %f", CountRightIter(3, 1.046667));
   assert(fabs(CountRightIter(3, 1.046667) - 0.748198) < 0.1);
   printf("Scooooooooooby-Doooooooby-Doooooo");
   return 0;
