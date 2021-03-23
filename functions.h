1d30b20a264294401549249c1e07cb1a1f2a5512
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2021-03-23 13:19:54 +0300

    Update functions.h

--- functions.h
+++ functions.h
@@ -1,7 +1,7 @@
 void clrscr();
-void presentData(float distance, unsigned int year, float value, float cost);
+void PresentData(float distance, unsigned int year, float value, float cost);
 void Menu();
-int EnterTheNumber(unsigned int number);
+unsigned int EnterTheNumber(unsigned int number);
 float CostOfTheDistillation(float distance);
 float CostOfTheCustoms(unsigned int year, float value);
 float TotalCostInByn(float cost, float distance, unsigned int year, float value);
