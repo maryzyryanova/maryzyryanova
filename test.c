7f2572aa3b50ff77b4badf7138f1a7db78a28e86
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2021-03-24 00:12:51 +0300

    Update test.c

--- test.c
+++ test.c
@@ -1,11 +1,12 @@
 #include <assert.h>
 #include <math.h>
+#include <stdio.h>
 #include "functions.h"
 
 #undef main
-int main()
-{
-    assert(fabs(CountRightRec(2, 30) - 0.48) < 0.01);
-    assert(fabs(CountRightIter(3, 60) - 0.75) < 0.01);
-    printf("Scooooooooooby-Doooooooby-Doooooo");
+int main() {
+  assert(fabs(CountRightRec(2, 30) - 0.48) < 0.01);
+  assert(fabs(CountRightIter(3, 60) - 0.75) < 0.01);
+  printf("Scooooooooooby-Doooooooby-Doooooo");
+  return 0;
 }
