c54b088d01e113ab71d28e111e7249cc684d1af8
ClGelver <75281453+MaximSelhanovich@users.noreply.github.com>
2021-03-24 07:47:37 +0300

    Update functions.c

--- functions.c
+++ functions.c
@@ -16,13 +16,13 @@ unsigned int CountFactorialRec(unsigned int n)
 
 unsigned int CountFactorialIter(unsigned int n) 
 {
+    int i, result = 1;
     if (n == 1 || n == 0)
     {
         return 1;    
     }
     else
     {
-        int i, result = 1;
         for (i = 1; i <= n; i++)
         {
             result *= (2*i - 1);
@@ -33,30 +33,30 @@ unsigned int CountFactorialIter(unsigned int n)
 
 float CountRightRec(unsigned int n, float angle)
 {
+    float result = 0.0;
     if (n == 0)
     {
         return 0;
     }
     else
     {
-        float result = 0.;
-        result += pow(-1, n - 1) * powf(angle, (2 * n) - 1) / CountFactorialRec(n);
+        result += pow(-1, n - 1) * pow(angle, (2 * n) - 1) / CountFactorialRec(n);
         return (result + CountRightRec(n - 1, angle));
     }
 }
 
 float CountRightIter(unsigned int n, float angle) 
 {
+    float i, right = 0.0;
     if (n == 0)
     {
         return 0;
     }
     else
     {
-        float i, right = 0.;
         for (i = 1.; i <= n; i++)
         {
-            right += pow(-1, (i - 1)) * powf(angle, ((2 * i) - 1)) / CountFactorialIter(i);
+            right += pow(-1, (i - 1)) * pow(angle, ((2 * i) - 1)) / CountFactorialIter(i);
         }
         return right;
     }
