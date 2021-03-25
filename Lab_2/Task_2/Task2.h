aef2de519b0e2098b803ee40203ac8655ce273c7
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2021-03-25 23:17:09 +0300

    Delete Lab_2 directory

--- Lab_2/Task_2/Task2.h
+++
@@ -1,8 +0,0 @@
-// Copyright 2021 Mary Zyryanova.
-#ifndef LAB_2_TASK_2_TASK2_H_
-#define LAB_2_TASK_2_TASK2_H_
-#endif  // LAB_2_TASK_2_TASK2_H_
-int64_t CountFactorialRec(int64_t n);
-int64_t CountFactorialIter(int64_t n);
-float CountRightRec(int64_t n, float angle);
-float CountRightIter(int64_t n, float angle);
