06a92347aa1ed5a7c3699a7484445b2d9d168311
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2021-04-07 01:16:32 +0300

    Update test.c

--- test.c
+++ test.c
@@ -7,8 +7,8 @@
 void test()
 {
     int array[3][4] = {(1, 1, 1, 0), (0, 0, 0, 0), (1, 1, 0, 1)};
-    assert(Index_Row(3, 4, array) == -1);
-    assert(Index_Column(3, 4, array) == 2);
+    assert(Index_Row(array, 4, 0) == -1);
+    assert(Index_Column(array, 3, 2) == 2);
     assert(Delete_Rows(3, 4, array) == 3);
     assert(Delete_Columns(3, 4, array) == 2);
 }
