6c62e675960e8d4efa8299cd82a86157f9f86f6f
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2021-04-07 00:59:01 +0300

    Add files via upload

---
+++ functions.c
@@ -0,0 +1,148 @@
+#include "functions.h"
+#include <stdbool.h>
+
+int Rows()
+{
+    int N;
+    printf("Rows: ");
+    while (scanf("%d", &N) != 1)
+    {
+        if (scanf("%d", &N) != 1) 
+        {
+            printf("Incorrect input, try again!\n");
+            printf("Rows: ");
+            while(getchar() != '\n') {}
+        }
+    }
+    return N;
+}
+
+int Columns()
+{
+    int M;
+    printf("Columns: ");
+    while (scanf("%d", &M) != 1)
+    {
+        if (scanf("%d", &M) != 1) 
+        {
+            printf("Incorrect input, try again!\n");
+            printf("Columns: ");
+            while(getchar() != '\n') {}
+        }
+    }
+    return M;
+}
+
+void Fill_Array(int N, int M, int **array)
+{
+    int i, j;
+    
+    for (i = 0; i < N; i++)
+    { 
+        array[i] = (int *)malloc(M * sizeof(int));
+    }
+
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < M; j++)
+        {
+            array[i][j] = rand() % 2;
+        }
+    }
+}
+
+void Print_Array(int N, int M, int **array)
+{
+    int i, j;
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < M; j++)
+        {
+            printf("%d", array[i][j]);
+            printf(" ");
+        }
+        printf("\n");
+    }  
+}
+
+int Index_Row(int ** array, int M, int i)
+{
+    int j, str = 0;
+    for (j = 0; j < M; j++)
+    {
+        if (array[i][j] == 1)
+        {
+            str++;
+        }
+    }
+
+    if (str == 1)
+    {
+        return i; 
+    } else {
+        return -1;
+    }
+}
+
+int Index_Column(int **array, int N, int j)
+{
+    int i, index, col = 0;
+    for (i = 0; i < N; i++)
+    {
+        if (array[i][j] == 1)
+        {
+            col++;
+        }
+    }
+    if (col == 1)
+    {
+        index = j;
+        return index; 
+    } else {
+        return -1;
+    }
+}
+
+int Delete_Rows(int N, int M, int **array)
+{
+    int i, j, row, wr = 0;
+    for (i = 0; i < N; ++i)
+    {
+        row = Index_Row(array, M, i);
+        printf("Row: %d\n", row);
+        bool allzero = true;
+        for (j = 0; j < M; ++j)
+        {
+            allzero = (row >= 0 && allzero);
+            array[wr][j] = array[i][j];
+        }
+        if (!allzero)
+        {
+            ++wr;
+        }
+    }
+    N = wr;
+    return N;
+}
+
+int Delete_Columns(int N, int M, int **array)
+{
+    int i, j, column, wr = 0;
+    for (j = 0; j < M; ++j)
+    {
+        column = Index_Column(array, N, j);
+        printf("Column: %d\n", column);
+        bool allzero = true;
+        for (i = 0; i < N; ++i)
+        {
+            allzero = (column >= 0 && allzero);
+            array[i][wr] = array[i][j];
+        }
+        if (!allzero)
+        {
+            ++wr;
+        }
+    }
+    M = wr;
+    return M;
+}
