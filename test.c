dda8f0c5305efee4b629edfdc2e069954c8deacb
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2021-04-07 01:24:47 +0300

    Update test.c

--- test.c
+++ test.c
@@ -1,16 +1,15 @@
 #include <assert.h>
 #include <math.h>
 #include <stdio.h>
-#include "task.c"
 #include "functions.h"
 
 void test()
 {
     int array[3][4] = {(1, 1, 1, 0), (0, 0, 0, 0), (1, 1, 0, 1)};
-    assert(Index_Row((*array), 4, 0) == -1);
-    assert(Index_Column((*array), 3, 2) == 2);
-    assert(Delete_Rows(3, 4, (*array)) == 3);
-    assert(Delete_Columns(3, 4, (*array)) == 2);
+    assert(Index_Row((**array), 4, 0) == -1);
+    assert(Index_Column((**array), 3, 2) == 2);
+    assert(Delete_Rows(3, 4, (**array)) == 3);
+    assert(Delete_Columns(3, 4, (**array)) == 2);
 }
 #undef main
 int main() {
