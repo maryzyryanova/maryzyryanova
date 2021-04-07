4048756a4d306f0e3b1d8420c9bb494c9f779b99
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2021-04-07 10:41:24 +0300

    Update test.c

--- test.c
+++ test.c
@@ -5,13 +5,11 @@
 
 void test()
 {
-    int array[3][4] = {(1, 1, 1, 0), (0, 0, 0, 0), (1, 1, 0, 1)};
-    int **pointer;
-    pointer = (int **)array;
-    assert(Index_Row(*(&pointer), 4, 0) == -1);
-    assert(Index_Column(*(&pointer), 3, 2) == 2);
-    assert(Delete_Rows(3, 4, *(&pointer)) == 3);
-    assert(Delete_Columns(3, 4, *(&pointer)) == 2);
+    int **array = {(1, 1, 1, 0), (0, 0, 0, 0), (1, 1, 0, 1)};
+    assert(Index_Row(*(&array), 4, 0) == -1);
+    assert(Index_Column(*(&array), 3, 2) == 2);
+    assert(Delete_Rows(3, 4, *(&array)) == 3);
+    assert(Delete_Columns(3, 4, *(&array)) == 2);
 }
 #undef main
 int main() {
