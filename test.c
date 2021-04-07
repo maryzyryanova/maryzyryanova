dfa47dd3cba376402424dca8f097082ce8191259
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2021-04-07 10:50:34 +0300

    Update test.c

--- test.c
+++ test.c
@@ -7,7 +7,7 @@ void test()
 {
     int array[3][4] = {(1, 1, 1, 0), (0, 0, 0, 0), (1, 1, 0, 1)};
     int **pointer;
-    pointer =(int **array)
+    pointer =(int **)array
     assert(Index_Row(*(&pointer), 4, 0) == -1);
     assert(Index_Column(*(&pointer), 3, 2) == 2);
     assert(Delete_Rows(3, 4, *(&pointer)) == 3);
