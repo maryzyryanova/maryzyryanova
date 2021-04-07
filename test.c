9a85cb53a5157bc9d8c4dc4330e6c44e7bc82511
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2021-04-08 02:44:04 +0300

    Update test.c

--- test.c
+++ test.c
@@ -10,16 +10,16 @@ void test()
 {
     int i;
     char** array = (char**)malloc(N * sizeof(char*));
-    for (i = 0; counter < N; i++) {
+    for (i = 0; i < N; i++) {
         array[i] = (char*)malloc(M * sizeof(char));
     }
     array = { 1, 1, 1, 0,
               0, 0, 0, 0,
               1, 1, 0, 1 };
-    assert(Index_Row(&array, N, 0) == -1);
-    assert(Index_Column(&array, M, 2) == 2);
-    assert(Delete_Rows(N, M, &array) == 3);
-    assert(Delete_Columns(N, M, &array) == 2);
+    assert(Index_Row(array, N, 0) == -1);
+    assert(Index_Column(array, M, 2) == 2);
+    assert(Delete_Rows(N, M, array) == 3);
+    assert(Delete_Columns(N, M, array) == 2);
 }
 #undef main
 int main() {
