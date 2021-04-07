f235e8c487576aa4089d51b30876835e56c3df9b
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2021-04-08 02:31:20 +0300

    Update test.c

--- test.c
+++ test.c
@@ -3,14 +3,16 @@
 #include <stdio.h>
 #include "functions.h"
 
+#define N 3
+#define M 4
 void test()
 {
     int *array;
-    array = (int*)malloc(3*4*sizeof(int)); 
-    assert(Index_Row(&array, 4, 0) == -1);
-    assert(Index_Column(&array, 3, 2) == 2);
-    assert(Delete_Rows(3, 4, &array) == 3);
-    assert(Delete_Columns(3, 4, &array) == 2);
+    array = (int*)malloc(N*M*sizeof(int)); 
+    assert(Index_Row(&array, M, 0) == -1);
+    assert(Index_Column(&array, N, 2) == 2);
+    assert(Delete_Rows(N, M, &array) == 3);
+    assert(Delete_Columns(N, M, &array) == 2);
 }
 #undef main
 int main() {
