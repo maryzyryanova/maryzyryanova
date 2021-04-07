e7f9350904012a8ffe4c6c55ffa1b9610deab668
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2021-04-07 11:26:02 +0300

    Update test.c

--- test.c
+++ test.c
@@ -7,10 +7,10 @@ void test()
 {
     int *array;
     array = (int*)malloc(3*4*sizeof(int));
-    assert(Index_Row(*(&array), 4, 0) == -1);
-    assert(Index_Column(*(&array), 3, 2) == 2);
-    assert(Delete_Rows(3, 4, *(&array)) == 3);
-    assert(Delete_Columns(3, 4, *(&array)) == 2);
+    assert(Index_Row(&array, 4, 0) == -1);
+    assert(Index_Column(&array, 3, 2) == 2);
+    assert(Delete_Rows(3, 4, &array) == 3);
+    assert(Delete_Columns(3, 4, &array) == 2);
 }
 #undef main
 int main() {
