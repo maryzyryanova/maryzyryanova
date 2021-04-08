a6aee216e587d7b922e0487ea623a8324c8b92d0
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2021-04-08 15:06:59 +0300

    Update test.c

--- test.c
+++ test.c
@@ -13,9 +13,37 @@ void test()
     int i;
     char test[MAX_STRING_SIZE];
     char** array = (char**)malloc(MAX_AMOUNT_OF_STRINGS * sizeof(char*));
-
-    for (i = 0; i < MAX_AMOUNT_OF_STRINGS ; i++) {
-        array[i] = (char*)malloc(MAX_STRING_SIZE * sizeof(char));
+    if(!array) 
+    {
+        printf("Error\n");
+        free(array);
+        array = NULL;
+        exit(1);
+    }
+    for (i = 0; i < MAX_AMOUNT_OF_STRINGS; i++)
+    { 
+        array[i] = (int *)malloc(MAX_STRING_SIZE * sizeof(int));
+    }
+    for (i = 0; i < MAX_AMOUNT_OF_STRINGS; i++)
+    {
+        if(!array[i]) 
+        {
+            printf("Error x2\n\n%d\n\n", i);
+            success = 0;
+            break;
+        }
+        if (success == 0) 
+        {
+            for (; i >= 0; i--) 
+            { 
+                free(array[i]);
+                printf("\n\n%d\n\n", i);
+            }
+            free(array);
+            printf("\n\n%d\n\n", i);
+            array = NULL;
+            exit(1);
+        }
     }
     
     if (!(input = fopen("List.txt", "r"))) {
