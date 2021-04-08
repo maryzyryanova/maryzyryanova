61e9fb7abed48a1c5ad0d0f715f49a4c8b8aa6cd
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2021-04-08 14:46:15 +0300

    Update test.c

--- test.c
+++ test.c
@@ -6,7 +6,29 @@
 
 void test()
 {
-    int array[3][4] = {(1, 1, 1, 0), (0, 0, 0, 0), (1, 1, 0, 1)};
+    int **array = (int**)malloc(3*sizeof(int *));
+    for (i = 0; i < 3; i++)
+    { 
+        array[i] = (int *)malloc(4 * sizeof(int));
+        if(!array[i]) 
+        {
+            printf("Error x2\n\n%d\n\n", i);
+            success = 0;
+            break;
+        }
+        if (success == 0) 
+        {
+            for (; i >= 0; i--) 
+            { 
+                free(array[i]);
+                printf("\n\n%d\n\n", i);
+            }
+            free(array);
+            printf("\n\n%d\n\n", i);
+            array = NULL;
+            exit(1);
+        }
+    }
     assert(Index_Row(array, 3, 4) == -1);
     assert(Index_Column(array, 3, 4) == 2);
     assert(Delete_Rows(3, 4, array) == 3);
