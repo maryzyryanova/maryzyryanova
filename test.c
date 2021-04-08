71cf55d1c325d2dc965bba3866c9d0139d869a77
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2021-04-08 12:36:54 +0300

    Update test.c

--- test.c
+++ test.c
@@ -11,7 +11,7 @@ void test()
     FILE* input;
     FILE* output;
     int i;
-    char* test[MAX_AMOUNT_OF_STRINGS];
+    char* test[MAX_STRING_SIZE];
     char** array = (char**)malloc(MAX_AMOUNT_OF_STRINGS * sizeof(char*));
 
     for (i = 0; i < MAX_AMOUNT_OF_STRINGS ; i++) {
