800612939b112d213fd3e8c7997807b7ed0a98ce
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2021-04-08 03:01:18 +0300

    Update test.c

--- test.c
+++ test.c
@@ -10,9 +10,7 @@ void test()
 {
     int i;
     int** array = (int**)malloc(N * sizeof(int*));
-    for (i = 0; i < N; i++) {
-        array[i] = (int*)malloc(M * sizeof(int));
-    }
+    FillArray(N, M, array);
     assert(Index_Row(array, N, 0) == -1);
     assert(Index_Column(array, M, 2) == 2);
     assert(Delete_Rows(N, M, array) == 3);
