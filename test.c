36259c3a90cb4c470613df348dee97d661eb22ed
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2021-04-08 18:35:57 +0300

    Update test.c

--- test.c
+++ test.c
@@ -32,8 +32,8 @@ void test()
         }
     }
     assert(Index_Row(array, M, 2) == -1);
-    assert(Index_Column(array, N, 3) == 3);
-    assert(Delete_Rows(N, M, array) == 2);
+    assert(Index_Column(array, N, 3) == 2);
+    assert(Delete_Rows(N, M, array) == 3);
     assert(Delete_Columns(2, M, array) == 2);
 }
 
