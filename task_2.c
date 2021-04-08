3f5fe17c35e2d0d485180ce135aae96ec7503125
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2021-04-08 15:19:08 +0300

    Update task_2.c

--- task_2.c
+++ task_2.c
@@ -14,31 +14,19 @@ int main()
     if(!array) 
     {
         printf("Error\n");
-        free(array);
-        array = NULL;
         exit(1);
     }
     for (i = 0; i < MAX_AMOUNT_OF_STRINGS; i++)
     { 
         array[i] = (char *)malloc(MAX_STRING_SIZE * sizeof(char));
-    }
-    for (i = 0; i < MAX_AMOUNT_OF_STRINGS; i++)
-    {
         if(!array[i]) 
         {
-            printf("Error x2\n\n%d\n\n", i);
-            success = 0;
-            break;
-        }
-        if (success == 0) 
-        {
-            for (; i >= 0; i--) 
+            for (i -= 1; i >= 0; i--) 
             { 
                 free(array[i]);
-                printf("\n\n%d\n\n", i);
+                array[i] = NULL;
             }
             free(array);
-            printf("\n\n%d\n\n", i);
             array = NULL;
             exit(1);
         }
