2d1249464a3d651e506fe75684e66d634c90226a
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2021-04-27 23:03:47 +0300

    find max length

---
+++ task.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "functions.h"
+
+int main()
+{
+    int N, i, length, sum = 0;
+    struct node* root;
+    root = NULL;
+    printf("Your numbers: ");
+    do
+    {
+        scanf("%d", &N);
+        root = Add(N, root);
+    }while(getchar() != '\n');
+    length = GetSum(root, sum);
+    Print(root);
+    printf("\nThe length of the longest branch: %d\nThe sum of the longest branch: %d", length, sum);
+    return 0;
+}
