d69ed87cc5701cc246393bd738999721f24679af
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2021-04-27 23:19:57 +0300

    edit functions

---
+++ functions.c
@@ -0,0 +1,74 @@
+#include "functions.h"
+
+struct node* New(){
+    struct node *list;
+
+    list = malloc(sizeof(struct node) * 1);
+    if(!list) 
+    {
+        printf("Error\n");
+        exit(1);
+    }
+
+    list->x = malloc(sizeof(int) * 100);
+    if(!list->x) 
+    {
+        printf("Error\n");
+        exit(1);
+    }
+
+    list->next = NULL;
+    return list;
+}
+
+struct node* PushBack(struct node *head, struct node* tail, int* size, int x) {
+    if(*size == 0)
+    {
+        head = tail = New();
+        *size += 1;
+        tail->x = x;
+        head->x = x;
+        return tail;
+    } else {
+        struct node* list = New();
+        *size += 1;
+        list->next = tail;
+        tail = list;
+        list->x = x;
+        return tail;
+    }
+}
+
+struct node* get(struct node *tail,int index) {
+    struct node* current = tail;
+    while(index--)
+    {
+     current = current -> next;
+    }
+    return current;
+}
+
+
+void Sort(struct node* tail, int size)
+{
+    for (int i = 0 ; i < size - 1; ++i)
+    {
+        for (int j = 0 ; j < size - 1 ; ++j)
+        {
+            if (get(tail,j)->x > get(tail,j+1)->x)
+            {
+                int temp = get(tail, j)->x;
+                get(tail, j)->x = get(tail, j+1)->x;
+                get(tail, j+1)->x = temp;
+            }
+        }
+    }
+}
+
+void Print (struct node* tail, int size){
+    for (int i = 0 ; i < size; ++i)
+    {
+        printf("%d",get(tail, i)->x);
+        printf("\n");
+    }
+}
--- functions.h
+++ functions.h
@@ -1,37 +1,10 @@
-int size = 0;
 struct node {
     struct node *next;
     int x;
 };
-struct node *head,*tail;
 
-struct node* New(){
-    size++;
-    struct node *v;
-    v = malloc(sizeof(struct node) * 1);
-    v->x = malloc(sizeof(int) * 100);
-    v -> next = NULL;
-    return v;
-}
-
-void push_r(int x) {
-    if(size == 0)
-    {
-        head = tail = New();
-        tail -> x = x;
-        head -> x = x;
-    } else {
-        struct node* v = New();
-        v->next = tail;
-        tail = v;
-        v->x = x;
-    }
-}
-
-struct node* get(int index) {
-    struct node * cur = tail;
-    while(index--) {
-        cur = cur -> next;
-    }
-    return cur;
-}
+struct node* New();
+struct node* PushBack(struct node *head, struct node* tail, int* size, int x);
+struct node* get(struct node *tail,int index);
+void Sort(struct node* tail, int size);
+void Print (struct node* tail, int size);
