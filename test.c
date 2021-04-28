33ace0b1708f3bba40ee9720cfc4daf4465a4cd2
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2021-04-28 22:48:55 +0300

    create makefile and test.c

---
+++ test.c
@@ -0,0 +1,42 @@
+#include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "functions.h"
+
+void Test()
+{
+    struct node* root;
+    int height;
+    int sum;
+    root = NULL;
+    root = Add(5, root);
+    root = Add(2, root);
+    root = Add(4, root);
+    root = Add(6, root);
+    root = Add(7, root);
+    root = Add(9, root);
+    height = Height(root, 0);
+    sum = Sum(root, 0);
+    assert(height == 3);
+    assert(sum == 20);
+
+    root = NULL;
+    root = Add(1, root);
+    root = Add(6, root);
+    root = Add(2, root);
+    root = Add(3, root);
+    root = Add(6, root);
+    root = Add(2, root);
+    root = Add(5, root);
+    height = Height(root, 0);
+    sum = Sum(root, 0);
+    assert(height == 4);
+    assert(sum == 7);
+}
+
+#undef main
+int main()
+{
+    Test();
+    return 0;
+}
