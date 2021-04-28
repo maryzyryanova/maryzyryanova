dfe30ad39fa8441eb02ec288546ce4af08ba9057
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2021-04-28 22:56:53 +0300

    edit file

--- test.c
+++ test.c
@@ -21,17 +21,19 @@ void Test()
     assert(sum == 20);
 
     root = NULL;
-    root = Add(1, root);
+    root = Add(2, root);
     root = Add(6, root);
     root = Add(2, root);
-    root = Add(3, root);
     root = Add(6, root);
+    root = Add(8, root);
+    root = Add(3, root);
+    root = Add(12, root);
     root = Add(2, root);
     root = Add(5, root);
     height = Height(root, 0);
     sum = Sum(root, 0);
-    assert(height == 4);
-    assert(sum == 6);
+    assert(height == 3);
+    assert(sum == 19);
 }
 
 #undef main
